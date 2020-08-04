fe589b6b402af0cbb9bc4cd594e6e934bec8afbf
YoungJun <coke05288@gmail.com>
2020-08-04 19:07:59 +0900

    Add Remark Parenthesis Test ***Error

--- StudyCpp/StudyCpp/3주차/parenthesis_test.cpp
+++ StudyCpp/StudyCpp/3주차/parenthesis_test.cpp
@@ -13,23 +13,27 @@ int main(void) {
 
 	cin >> question;
 
+	// 주어진 문제 문자열에서 괄호가 잘닫혀있는지에 대해 True/False로 반환해주는 알고리즘. ****현재 오류 있음****
 	for (int i = 0; i < question.length(); i++) {
-		if (question.at(i) == '(' || question.at(i) == '{' || question.at(i) == '[') {
+		if (question.at(i) == '(' || question.at(i) == '{' || question.at(i) == '[') { // 앞 괄호 발견 시 스택에 push().
 			s.push(question.at(i));
 		}
-		else if (question.at(i) == ')' || question.at(i) == '}' || question.at(i) == ']') {
-			if (!s.empty()) {
-				switch (question.at(i)) {
-				case ')':
+		else if (question.at(i) == ')' || question.at(i) == '}' || question.at(i) == ']') { // 뒷 괄호 발견 시 괄호 모양에 따라 스택과 비교.
+			if (!s.empty()) { // 스택 공백 체크
+				switch (question.at(i)) { 
+				case ')': 
 					if (s.top() == '(') answer = "True";
+					s.pop();
 					break;
 
 				case '}':
 					if (s.top() == '{') answer = "True";
+					s.pop();
 					break;
 
 				case ']':
 					if (s.top() == '[') answer = "True";
+					s.pop();
 					break;
 
 				default:
