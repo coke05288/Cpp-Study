77c1b67adab007372afbf3c8e9d7af80e7df3526
YoungJun <coke05288@gmail.com>
2020-08-19 02:19:33 +0900

    StudyCpp week 6 | Programmers 더 맵게

---
+++ StudyCpp/StudyCpp/6주차/programmers_더 맵게.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+	int answer = 0;
+
+	//< data type, <associative container>, comparsion function class >
+	priority_queue< int, vector<int>, greater<int> > newcoville;
+
+	for (int i = 0; i < scoville.size(); i++) newcoville.push(scoville[i]);
+
+	while (newcoville.top() < K && newcoville.size() > 1) {
+		int first, second;
+
+		first = newcoville.top();
+		newcoville.pop();
+
+		second = newcoville.top();
+		newcoville.pop();
+
+		newcoville.push(second * 2 + first);
+
+		answer++;
+	}
+
+	if (newcoville.top() < K) {
+		return -1;
+	}
+
+	return answer;
+}
+
+int main() {
+
+	vector<int> scoville = { 1, 2, 3, 9, 10, 12 };
+
+	int K = 7;
+
+	int answer =  solution(scoville, K);
+
+	cout << answer << endl;
+
+	return 0;
+}
